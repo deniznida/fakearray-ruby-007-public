058e83c7adc22f8446bf680d7f96dac0100aabc9
deniznida <denizunat@hotmail.com>
2015-02-14 17:45:03 -0500

    done

--- lib/fake_array.rb
+++ lib/fake_array.rb
@@ -2,6 +2,21 @@ class FakeArray
 
   # you'll need a splat in this class somewhere
 
+  def initialize(*args)
+    @elements = args
+  end
 
+  def [](index)
+    @elements[index]
+  end
 
+  def each
+    @elements.each do |i|
+      yield i
+    end
+  end
+
+  def first
+    @elements[0]
+  end
 end
